6638b8e1f7ee015ff29356078efeb30dd5a9db85
subhankhan <subhanakbar908@gmail.com>
2025-11-24 06:24:36 -0500

    "Task1.1"

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,25 @@
 package theater;
 
+/**
+ * Represents a play with a name and type.
+ *
+ * @null This class does not accept or return null values.
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private final String name;
+    private final String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
