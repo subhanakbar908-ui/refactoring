4575dcbecf3ac783f8f429b0408afa55be0ca507
subhankhan <subhanakbar908@gmail.com>
2025-11-24 07:04:42 -0500

    "Task 2.3"

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -31,8 +31,6 @@ public class StatementPrinter {
         final StringBuilder result = new StringBuilder(
                 "Statement for " + invoice.getCustomer() + System.lineSeparator());
 
-        final NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
-
         for (Performance performance : invoice.getPerformances()) {
 
             // add volume credits
@@ -42,7 +40,7 @@ public class StatementPrinter {
             result.append(String.format(
                     "  %s: %s (%s seats)%n",
                     getPlay(performance).getName(),
-                    frmt.format(getAmount(performance) / Constants.PERCENT_FACTOR),
+                    usd(getAmount(performance)),
                     performance.getAudience()));
 
             totalAmount += getAmount(performance);
@@ -50,7 +48,7 @@ public class StatementPrinter {
 
         result.append(String.format(
                 "Amount owed is %s%n",
-                frmt.format(totalAmount / Constants.PERCENT_FACTOR)));
+                usd(totalAmount)));
         result.append(String.format(
                 "You earned %s credits%n",
                 volumeCredits));
@@ -106,6 +104,17 @@ public class StatementPrinter {
         }
 
         return result;
+    }
+
+    /**
+     * Formats the given amount (in cents) as a US dollar currency string.
+     *
+     * @param amountCents the amount in cents
+     * @return the formatted amount in US dollars
+     */
+    private String usd(int amountCents) {
+        final NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
+        return frmt.format(amountCents / (double) Constants.PERCENT_FACTOR);
     }
 
     private Play getPlay(Performance performance) {
